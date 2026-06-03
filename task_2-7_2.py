31d3aa5797ca79e2e516b328dad48e5ade9cf2d4
aesmater <sonyaaa2811@gmail.com>
2026-06-03 23:07:50 +0300

    Delete task_2-7_2.py

--- task_2-7_2.py
+++
@@ -1,9 +0,0 @@
-sequences = ["ATATACGCGTA", "CTTCGGNGGA"]
-for seq in sequences:
-    print("Последовательность целиком:")
-    print(seq)
-    print("Та же последовательность построчно:")
-    for nucleotide in seq:
-        print(nucleotide)
-    print("-" * 20) 
-print("Цикл выполнен")
